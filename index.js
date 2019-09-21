9e22b4e6ff548647dd1c78122aa071030e987a82
Mursaleen Taqi <leemursa@gmail.com>
2019-09-21 12:34:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const prepend =["Milo","Otis","Garfield"];
 const removeLast =["Milo","Otis","Garfield"];
 const removeFirst =["Milo","Otis","Garfield"];
 prepend.unshift("Odie");
+append.push("Odie");
 removeLast.pop();
 removeFirst.shift("Milo");
